d37fcebd4b948127d914657e280569bab99f2120
Mehdi Benrefad <benrefadmehdi@gmail.com>
2021-05-06 20:14:46 +0000

    Initial Commit

---
+++ sideMenuApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sideMenuApplication
+//
+//  Created by Mehdi Benrefad on 06/05/2021.
+//  Copyright © 2021 Mehdi Benrefad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
