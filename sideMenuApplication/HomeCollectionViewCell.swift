f7e7fb159eae65885b5bf4ef103ef4cbbeaaa388
Mehdi Benrefad <benrefadmehdi@gmail.com>
2021-05-07 17:40:19 +0000

    laptops

---
+++ sideMenuApplication/HomeCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  HomeCollectionViewCell.swift
+//  sideMenuApplication
+//
+//  Created by Mehdi Benrefad on 07/05/2021.
+//  Copyright © 2021 Mehdi Benrefad. All rights reserved.
+//
+
+import UIKit
+
+class HomeCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productPhoto: UIImageView!
+}
--- sideMenuApplication/ViewController.swift
+++ sideMenuApplication/ViewController.swift
@@ -8,13 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController , UICollectionViewDelegate , UICollectionViewDataSource , UICollectionViewDelegateFlowLayout{
+    
     @IBOutlet weak var buttonToMenu: UIBarButtonItem!
+    @IBOutlet weak var collectionView: UICollectionView!
+    var laptops = [
+    UIImage(named: "Dog"),
+    UIImage(named: "Dog"),
+    UIImage(named: "Dog"),
+    UIImage(named: "Dog")
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        collectionView.delegate = self
+        collectionView.dataSource = self
     }
 
     @IBAction func swipeHandler(_ gestureRecognizer : UISwipeGestureRecognizer) {
@@ -26,6 +35,22 @@ class ViewController: UIViewController {
             */
         }
         
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return laptops.count
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "laptopsCell", for: indexPath)as! HomeCollectionViewCell
+        cell.productPhoto.image = laptops[indexPath.row]
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
     }
 }
 
