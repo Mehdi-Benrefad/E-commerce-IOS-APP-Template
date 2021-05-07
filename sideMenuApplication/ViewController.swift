858869b961954ef6dbb3e2193586a7a5327948d8
Mehdi Benrefad <benrefadmehdi@gmail.com>
2021-05-07 19:13:36 +0000

    animate transition

--- sideMenuApplication/ViewController.swift
+++ sideMenuApplication/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController , UICollectionViewDelegate , UICollectionViewDataSource , UICollectionViewDelegateFlowLayout{
     
+    @IBOutlet weak var pageControl: UIPageControl!
     @IBOutlet weak var buttonToMenu: UIBarButtonItem!
     @IBOutlet weak var collectionView: UICollectionView!
     var laptops = [
@@ -24,6 +25,9 @@ class ViewController: UIViewController , UICollectionViewDelegate , UICollection
         // Do any additional setup after loading the view.
         collectionView.delegate = self
         collectionView.dataSource = self
+        pageControl.numberOfPages = laptops.count
+        //passer automatiquement d'une image a une autre avec timer
+        startTimer()
     }
 
     @IBAction func swipeHandler(_ gestureRecognizer : UISwipeGestureRecognizer) {
@@ -55,6 +59,25 @@ class ViewController: UIViewController , UICollectionViewDelegate , UICollection
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return 0
+    }
+    
+    
+    //Automatisation dupassage d'unproduit a un autre
+    var timer:Timer?
+    var currentIndex = 0
+    
+    func startTimer(){
+        timer = Timer.scheduledTimer(timeInterval: 3, target: self, selector: #selector(moveToNextIndex), userInfo: nil, repeats: true)
+    }
+    
+    @objc func moveToNextIndex(){
+        if currentIndex < laptops.count - 1 {
+            currentIndex += 1
+        }else{
+            currentIndex = 0
+        }
+        collectionView.scrollToItem(at: IndexPath(item: currentIndex, section: 0), at: .centeredHorizontally, animated: true)
+        pageControl.currentPage = currentIndex
     }
 }
 
