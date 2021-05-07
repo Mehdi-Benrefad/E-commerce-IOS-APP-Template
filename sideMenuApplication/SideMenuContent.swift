bb6449673a3cd2796402bb694073c78cab7b7162
Mehdi Benrefad <benrefadmehdi@gmail.com>
2021-05-07 00:29:57 +0000

    model of the menu

---
+++ sideMenuApplication/SideMenuContent.swift
@@ -0,0 +1,21 @@
+//
+//  SideMenuContent.swift
+//  sideMenuApplication
+//
+//  Created by Mehdi Benrefad on 07/05/2021.
+//  Copyright © 2021 Mehdi Benrefad. All rights reserved.
+//
+
+import Foundation
+
+struct SideMenu{
+    var image = String()
+    var libele = String()
+}
+
+var list = [
+            SideMenu(image:"",libele:""),
+            SideMenu(image:"",libele:""),
+            SideMenu(image:"",libele:""),
+            SideMenu(image:"",libele:"")
+            ]
